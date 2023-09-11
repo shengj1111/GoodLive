5b82bc4d63c89804ff1b3c5866cf9da6e96937a7
shengji <1832721150@qq.com>
2023-09-11 17:04:43 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 export default function App() {
   return (
-    <div className='aa'>App</div>
+    <div>App</div>
   )
 }
---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+import request from "../utils/request";
+
+/**
+ * 路径地址
+ */
+const base = {}
+
+/**
+ * 请求方法
+ */
+const api = {}
+
+export default api
